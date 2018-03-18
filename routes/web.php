0c8a66e5fbb3185ab01d37a5494bc238d44c1306
Wang Ling <wangling@506539242.com>
2018-03-18 20:22:22 +0800

    first

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', 'StaticPagesController@home');
+////Route::get('/help', 'StaticPagesController@help');
+//Route::get('/about', 'StaticPagesController@about');
+//Route::get('/help', 'StaticPagesController@help')->name('help');
+//Route::get('/faq', 'StaticPagesController@help')->name('help');
+
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
+
+Route::get('signup', 'UsersController@create')->name('signup');
